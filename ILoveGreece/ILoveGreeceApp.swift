2af1609de40d8bf1962d9e24513149662dbaba5e
Dusan-O <dusan.orescanin@gmail.com>
2022-08-26 20:22:34 +0200

    Initial Commit

---
+++ ILoveGreece/ILoveGreeceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ILoveGreeceApp.swift
+//  ILoveGreece
+//
+//  Created by Dusan Orescanin on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ILoveGreeceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
